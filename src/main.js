e356670990e3b0712e74db0fb04c5a57805edc7c
Inga <engaezik@gmail.com>
2022-06-17 11:49:41 +0300

    upgrade html

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ const messages = document.getElementById('messages');
 socket.on('chat message', function (msg) {
     var item = document.createElement('li');
     item.textContent = msg;
+    item.classList.add("list-group-item")
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
@@ -15,6 +16,7 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 
 form.addEventListener('submit', function (e) {
+    console.log("form")
     e.preventDefault();
     if (input.value) {
         socket.emit('chat message', input.value);
